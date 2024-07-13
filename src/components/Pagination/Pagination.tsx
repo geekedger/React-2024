e31f2544c833d925fd35dfe9db264966b4568989
geekedger <geekedger@gmail.com>
2024-07-13 14:37:46 +0300

    fix: adjust styles

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,6 +1,4 @@
-// components/Pagination/Pagination.tsx
 import React from 'react';
-// import '@testing-library/jest-dom'
 import { useSearchParams } from 'react-router-dom';
 import './Pagination.css';
 
